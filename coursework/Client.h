1b59107a253c726c3221ee117c333f7f39a34a52
Stepan Lozhkin <stepanl123@yandex.ru>
2026-04-29 22:46:48 +0300

    upd: Client.h

--- coursework/Client.h
+++ coursework/Client.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 using namespace std;
+
 class Client{
 private:
     string clearingID;
@@ -14,14 +15,24 @@ private:
 public:
     Client();
     Client(string clearingID, string firmName, string address);
+
     void addBankID(string ID);
     void addAsset(string name, int value);
-    void resetBalance;
+    void resetBalance();
     void changeBalance(int diff);
     bool deficit();
     int totalPledgeValue();
+
     string getClearingId();
     string getFirmName();
     string getAddress();
     int getBalance();
-}
+
+    // Для сохранения/загрузки
+    const vector<string>& getBankIDs() const;
+    const vector<string>& getAssets() const;
+    const vector<int>& getAssetValues() const;
+    void setBalance(int b);
+    string toCSV() const;
+    static Client fromCSV(string line);
+};
